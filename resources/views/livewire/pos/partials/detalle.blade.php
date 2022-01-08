4a25971654ca07770c95eadbb7086ea0460c0e7d
andrestrocel44@gmail.com <andrestrocel44@gmail.com>
2022-01-08 13:19:10 -0400

    Retoques 1.3

--- resources/views/livewire/pos/partials/detalle.blade.php
+++ resources/views/livewire/pos/partials/detalle.blade.php
@@ -1,88 +1,105 @@
 
-<div class="connect-sorting">
-<div class="connect-sorting-content">
-    <div class="card simple-title-task ui-sortable-handle">
-        <div class="card-body">
 
-        @if($total > 0)
-        <div class="table-responsive tblscroll" style="max-height: 650px; overflow:hidden">
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        @livewireStyles
+    </head>
+    <body>
+        
+                
+        <div class="connect-sorting">
+        <div class="connect-sorting-content">
+            <div class="card simple-title-task ui-sortable-handle">
+                <div class="card-body">
 
-            <table class="table table-dark table-flush mt-1">
-                <thead class=" thead-dark text-white " >
-                    <tr>
-                        <th width="10%"></th>
-                        <th class="table-th text-left text-white">DESCRIPCIÓN</th>
-                        <th class="table-th text-center text-white">PRECIO</th>
-                        <th  width="13%"class="table-th text-center text-white">CANTIDAD</th>
-                        <th class="table-th text-center text-white">IMPORTE</th>
-                        <th class="table-th text-center text-white">ACCIONES</th>
+                @if($total > 0)
+                <div class="table-responsive tblscroll" style="max-height: 650px; overflow:hidden">
 
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($cart as $item)
-                    <tr>
-                        <td class="text-center table-th">
-                            @if(count($item->attributes) > 0)
-                            <span>
-                                <img src="{{asset('storage/producto/' .$item->attributes[0])}}" alt="Imàgen de producto" height="90" width="90px" class="rounded">
-                            </span>
-                            @endif
-                        </td>
-                        <td><h6>{{$item->nombre}}</h6></td>
-                        <td class="text-center">Bs{{number_format($item->precio,2)}}</td>
-                        <td>
-                            <input type="number" name="" id="r{{$item->id}}"
-                                wire:change="updateQty({{$item->id}}, $('#r' + {{$item->id}}).val() )"
-                                style="font-size: 1rem!important"
-                                class="form-control text-center"
-                                value="{{$item->cantidad}}"
-                            >
-                        </td>
-                        <td class="text-center">
-                            <h6>
-                                Bs{{number_format($item->precio * $item->cantidad,2)}}
-                            </h6>
-                        </td>
-                        <td class="text-center">
-                        @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-danger btn-sm" 
-                            data-toggle="modal" data-target="#ModalDeleteDetalle{{$item->id}}" 
-                        ><a></a><i class="fa fa-fw fa-trash"></i></button>   
+                    <table class="table table-dark table-flush mt-1">
+                        <thead class=" thead-dark text-white " >
+                            <tr>
+                                <th width="10%"></th>
+                                <th class="table-th text-left text-white">DESCRIPCIÓN</th>
+                                <th class="table-th text-center text-white">PRECIO</th>
+                                <th  width="13%"class="table-th text-center text-white">CANTIDAD</th>
+                                <th class="table-th text-center text-white">IMPORTE</th>
+                                <th class="table-th text-center text-white">ACCIONES</th>
 
-                        <button wire:click.prevent="decreaseQty({{$item->id}})" class="btn btn-dark mbmobile">
-                            <i class="fas fa-minus"></i>
-                        </button>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($cart as $item)
+                            <tr>
+                                <td class="text-center table-th">
+                                    @if(count($item->attributes) > 0)
+                                    <span>
+                                        <img src="{{asset('storage/producto/' .$item->attributes[0])}}" alt="Imàgen de producto" height="90" width="90px" class="rounded">
+                                    </span>
+                                    @endif
+                                </td>
+                                <td><h6>{{$item->nombre}}</h6></td>
+                                <td class="text-center">Bs{{number_format($item->precio,2)}}</td>
+                                <td>
+                                    <input type="number" name="" id="r{{$item->id}}"
+                                        wire:change="updateQty({{$item->id}}, $('#r' + {{$item->id}}).val() )"
+                                        style="font-size: 1rem!important"
+                                        class="form-control text-center"
+                                        value="{{$item->cantidad}}"
+                                    >
+                                </td>
+                                <td class="text-center">
+                                    <h6>
+                                        Bs{{number_format($item->precio * $item->cantidad,2)}}
+                                    </h6>
+                                </td>
+                                <td class="text-center">
+                                @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger btn-sm" 
+                                    data-toggle="modal" data-target="#ModalDeleteDetalle{{$item->id}}" 
+                                ><a></a><i class="fa fa-fw fa-trash"></i></button>   
 
-                        <button wire:click.prevent="increaseQty({{$item->id}})" class="btn btn-dark mbmobile">
-                            <i class="fas fa-minus"></i>
-                        </button>
-                        
-                        
-                        </td>                                                   
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
+                                <button wire:click.prevent="decreaseQty({{$item->id}})" class="btn btn-dark mbmobile">
+                                    <i class="fas fa-minus"></i>
+                                </button>
+
+                                <button wire:click.prevent="increaseQty({{$item->id}})" class="btn btn-dark mbmobile">
+                                    <i class="fas fa-minus"></i>
+                                </button>
+                                
+                                
+                                </td>                                                   
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+                @else
+                <div style="border:5px solid ;border-radius:10px;">
+                <h3 class="text-center text-muted">Agregar productos a la venta</h3>
+                </div>
+            
+                @endif
+            
+            <div  wire:loading.inline wire:target="saveSale"  >
+                    <h4 class="text-danger text-center">Guardando Venta...</h4>
+                </div>
+            
+            
+            
+            
+
+                </div>
+            </div>
         </div>
-        @else
-        <div style="border:5px solid ;border-radius:10px;">
-        <h3 class="text-center text-muted">Agregar productos a la venta</h3>
-        </div>
-       
-        @endif
-     
-      <div  wire:loading.inline wire:target="saveSale"  >
-            <h4 class="text-danger text-center">Guardando Venta...</h4>
-        </div>
-      
-       
-      
-       
 
         </div>
-    </div>
-</div>
 
-</div>
+    @livewireScripts
+    </body>
+    </html>
+  
--- resources/views/livewire/pos/partials/total.blade.php
+++ resources/views/livewire/pos/partials/total.blade.php
@@ -1,26 +1,42 @@
 
-<div class="row" >
-    <div class="col-sm-11" style="background:#386EB8; border:5px solid ;border-radius:10px;">
-        <div>
-            <div class="connect-sorting" >
-                <h5 class="text-center text-white mb-3 ">RESUMEN DE VENTA</h5>
-            </div>
-            <div class="connect-sorting-content" >
-                <div class="card simple-title-task ui-sortable-handle">
-                    <div class="card-body">
-                        <div class="task-header">
-                            <div>
-                                <h2>TOTAL: Bs {{number_format($total,2)}}</h2>
-                                <input type="hidden" id="hiddenTotal" value="{{$total}}">
-                            </div>
-                            <div>
-                                <h4 class="mt-3">Articulos: {{$itemsQuantity}}</h4>
-                            </div>
-                        </div>
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        @livewireStyles
+    </head>
+    <body>
+        
+        <div class="row" >
+            <div class="col-sm-11" style="background:#386EB8; border:5px solid ;border-radius:10px;">
+                <div>
+                    <div class="connect-sorting" >
+                        <h5 class="text-center text-white mb-3 ">RESUMEN DE VENTA</h5>
                     </div>
+                    <div class="connect-sorting-content" >
+                        <div class="card simple-title-task ui-sortable-handle">
+                            <div class="card-body">
+                                <div class="task-header">
+                                    <div>
+                                        <h2>TOTAL: Bs {{number_format($total,2)}}</h2>
+                                        <input type="hidden" id="hiddenTotal" value="{{$total}}">
+                                    </div>
+                                    <div>
+                                        <h4 class="mt-3">Articulos: {{$itemsQuantity}}</h4>
+                                    </div>
+                                </div>
+                            </div>
 
-                </div><br>
-            </div>          
+                        </div><br>
+                    </div>          
+                </div>
+            </div>
         </div>
-    </div>
-</div>
+
+
+    @livewireScripts
+    </body>
+    </html>
+  
